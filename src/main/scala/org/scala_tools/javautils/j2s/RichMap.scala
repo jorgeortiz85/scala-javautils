96e795f898518aa51e335d8810f7d457f73224e8
Jorge Ortiz <jeortiz@jorge-ortizs-macbook-pro.local>
2009-04-06 01:07:52 -0700

    LICENSE, README, Copyright notices

--- src/main/scala/org/scala_tools/javautils/j2s/RichMap.scala
+++ src/main/scala/org/scala_tools/javautils/j2s/RichMap.scala
@@ -1,7 +1,23 @@
+/**
+ * Copyright 2009 Jorge Ortiz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ *
+ **/
 package org.scala_tools.javautils.j2s
 
 import java.util.Map
-import scala.collection.jcl.{MapWrapper, Conversions}
+import scala.collection.jcl.{Conversions, MapWrapper}
 import scala.collection.mutable.ArrayBuffer
 import scala.Function.untupled
 
--- src/main/scala/org/scala_tools/javautils/j2s/RichSet.scala
+++ src/main/scala/org/scala_tools/javautils/j2s/RichSet.scala
@@ -1,7 +1,23 @@
+/**
+ * Copyright 2009 Jorge Ortiz
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License. 
+ *
+ **/
 package org.scala_tools.javautils.j2s
 
 import java.util.Set
-import scala.collection.jcl.{SetWrapper, Conversions}
+import scala.collection.jcl.{Conversions, SetWrapper}
 
 abstract class RichSet[T, C[U] <: Set[U]](set: C[T]) extends RichCollection[T, C](set) {
   override def toScala: SetWrapper[T] = Conversions.convertSet(set)
